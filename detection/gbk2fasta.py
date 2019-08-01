e63256e681bb900ae04a027307a1d6ee41587177
surh <sur00mx@gmail.com>
2019-08-01 15:03:16 -0700

    gbk to fasta

---
+++ detection/gbk2fasta.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# Copyright (C) 2019 Sur Herrera Paredes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from Bio import SeqIO
+from Bio import SeqRecord
+import argparse
+import os
+
+
+def process_arguments():
+    # Read arguments
+    parser_format = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=parser_format)
+    required = parser.add_argument_group("Required arguments")
+
+    # Define description
+    parser.description = ("Convert gbk to fasta.")
+
+    # Define required arguments
+    required.add_argument("--input", help=("Input filename in genbank"),
+                          required=True, type=str)
+
+    # Define other arguments
+    parser.add_argument("--output", help=("Output filename. If empty, the "
+                                          "output will be "
+                                          "<basename>.fasta"),
+                        type=str,
+                        default="")
+
+    # Read arguments
+    print("Reading arguments")
+    args = parser.parse_args()
+
+    # Processing goes here if needed
+
+    return args
+
+
+if __name__ == "__main__":
+    args = process_arguments()
+    i = 0
+    for record in SeqIO.parse(args.input, args.format):
+        if i > 0:
+            raise ValueError("There should be only one record in your input. "
+                             "Only the first record was processed.")
+        print(record.id)
+        with open(args.output, 'w') as oh:
+            SeqIO.write(record, oh, "fasta")
+        oh.close()
+        i = i + 1
