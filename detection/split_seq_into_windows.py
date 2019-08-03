8314009c99097ff3986a9ffb03810a17eec11cca
mgschiavon <mgschiavon@gmail.com>
2019-08-02 21:48:05 -0700

    Adding comment

--- detection/split_seq_into_windows.py
+++ detection/split_seq_into_windows.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
+# Load libraries
 from Bio import SeqIO
 from Bio import SeqRecord
 import argparse
